22e1bde066df8a5983f0874ec2266a6560518939
“koichi-utebi <k.utebi.1225@gmail.com>
2022-05-09 10:04:49 +0000

    8c false

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,11 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
 
-  resources :groups, except: [:destroy]
+  resources :groups do
+    get "join" => "groups#join"
+  end
+
   get 'search' => "searches#search", as: 'search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
 
+end
